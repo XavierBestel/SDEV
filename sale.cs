1328a6a492ddd0c8c01d006e794cbdb2ee16ae6a
Xavier Bestel <48968807+XavierBestel@users.noreply.github.com>
2022-05-03 10:24:45 +1000

    Add files via upload

---
+++ sale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u301_prac_03
+{
+    internal class sale
+    {
+        public string Textbook { get; internal set; }
+        public object Subject { get; internal set; }
+        public string Category { get; set; }
+
+        public object PurchasedPrice { get; internal set; }
+
+        public object SalePrice { get; internal set; }
+
+        public string Rating { get; internal set; }
+
+    }
+}
