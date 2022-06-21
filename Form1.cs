ebd939a284ee45c9b4b8b4edb01e34c7890b5311
Xavier Bestel <48968807+XavierBestel@users.noreply.github.com>
2022-06-21 10:12:32 +1000

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,59 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Actual_S.A.C
+namespace SAC_Task_2
 {
     public partial class Form1 : Form
     {
-        float total = 0f;
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void resetInputs()
-        {
-            numAge.Value = 0;
-            numAmount.Value = 0;
-
-        }
-
-        private void btnCalculate_Click(object sender, EventArgs e)
-        {
-            int age = (int)numAge.Value;
-            float purchasedPrice = (float)numAmount.Value;
-            float total = 0f; 
-
-            if(age == 0 | purchasedPrice == 0.0)
-            {
-                lblAmount.Text = "Enter Proper Amount";
-            }
-            else
-            {
-                float currentValue = CalculateCurrentValue(purchasedPrice, age);
-                total += currentValue;
-                lblAmount.Text = $"it is worth {currentValue}.\n the Colllection so far is Worth {total}";
-            }
-
-            resetInputs();
-
-        }
-
-        private float CalculateCurrentValue(float purchasedValue, int age)
-        {
-            float depriciation = purchasedValue * 0.2f * age;
-            if (depriciation > purchasedValue) return 0f;
-            return purchasedValue - depriciation; 
-        }
-        private void numAmount_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnQoute_Click(object sender, EventArgs e)
-        {
-            resetInputs();
-            lblAmount.Text = "";  
         }
     }
 }
